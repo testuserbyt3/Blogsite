6de4abf398452163f0f7dc3501d3a8a9375f7b73
mohammadhamzailtaf <144703210+mohammadhamzailtaf@users.noreply.github.com>
2023-09-15 23:32:45 -0700

    Initial commit

---
+++ src/screens/Index/Index.jsx
@@ -0,0 +1,365 @@
+import React from "react";
+import "./style.css";
+
+export const Index = () => {
+  return (
+    <div className="index">
+      <div className="div">
+        <img
+          className="icon-material"
+          alt="Icon material"
+          src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6502d34e72965ae6a063862d/img/icon-material-navigate-next.svg"
+        />
+        <div className="text-wrapper">Xiaomi 13 Pro 5G</div>
+        <img
+          className="cart-and-chat-icon"
+          alt="Cart and chat icon"
+          src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6502d34e72965ae6a063862d/img/cart-and-chat-icon-removebg-preview.png"
+        />
+        <div className="overlap">
+          <img
+            className="icon-awesome-rupee"
+            alt="Icon awesome rupee"
+            src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6505495cca82d56db149fab0/img/icon-awesome-rupee-sign.svg"
+          />
+          <img
+            className="screenshot"
+            alt="Screenshot"
+            src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6502d34e72965ae6a063862d/img/screenshot-2-removebg-preview.png"
+          />
+          <div className="overlap-group">
+            <img
+              className="line"
+              alt="Line"
+              src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6502d34e72965ae6a063862d/img/line-2.svg"
+            />
+            <img
+              className="img"
+              alt="Line"
+              src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6502d34e72965ae6a063862d/img/line-3.svg"
+            />
+            <img
+              className="line-2"
+              alt="Line"
+              src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6502d34e72965ae6a063862d/img/line-3.svg"
+            />
+            <img
+              className="line-3"
+              alt="Line"
+              src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6502d34e72965ae6a063862d/img/line-3.svg"
+            />
+            <img
+              className="line-4"
+              alt="Line"
+              src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6502d34e72965ae6a063862d/img/line-3.svg"
+            />
+          </div>
+          <div className="text-wrapper-2">4,999</div>
+          <div className="text-wrapper-3">₹79,999</div>
+          <div className="text-wrapper-4">Xiaomi 13 Pro 5G</div>
+        </div>
+        <div className="overlap-2">
+          <p className="p">12 GB + 256 GB</p>
+          <div className="component">
+            <div className="rectangle-wrapper">
+              <div className="rectangle" />
+            </div>
+            <div className="text-wrapper-5">Ceramic Black</div>
+          </div>
+          <div className="component-2">
+            <div className="div-wrapper">
+              <div className="rectangle-2" />
+            </div>
+            <div className="text-wrapper-6">Ceramic White</div>
+          </div>
+        </div>
+        <div className="overlap-3">
+          <div className="rectangle-3" />
+          <div className="icon-material-done-wrapper">
+            <img
+              className="icon-material-done"
+              alt="Icon material done"
+              src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6502d34e72965ae6a063862d/img/icon-material-done.svg"
+            />
+          </div>
+          <div className="img-wrapper">
+            <img
+              className="icon-material-done"
+              alt="Icon material done"
+              src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6502d34e72965ae6a063862d/img/icon-material-done.svg"
+            />
+          </div>
+          <div className="mi-screen-protect">Mi Screen Protect</div>
+          <div className="component-3">
+            <div className="text-wrapper-7">10 Days Replacement Policy</div>
+          </div>
+          <div className="element-years-warranty">2 Years Warranty</div>
+          <div className="component-4">
+            <img
+              className="icon-material-done"
+              alt="Icon material done"
+              src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6502d34e72965ae6a063862d/img/icon-material-done.svg"
+            />
+          </div>
+          <div className="complete-protection">Complete Protection</div>
+        </div>
+        <div className="overlap-4">
+          <img
+            className="icon-awesome-star"
+            alt="Icon awesome star"
+            src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6505495cca82d56db149fab0/img/icon-awesome-star.svg"
+          />
+          <img
+            className="icon-awesome-star-2"
+            alt="Icon awesome star"
+            src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6505495cca82d56db149fab0/img/icon-awesome-star-1.svg"
+          />
+          <img
+            className="icon-awesome-star-3"
+            alt="Icon awesome star"
+            src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6502d34e72965ae6a063862d/img/icon-awesome-star-1.svg"
+          />
+          <img
+            className="icon-awesome-star-4"
+            alt="Icon awesome star"
+            src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6502d34e72965ae6a063862d/img/icon-awesome-star-1.svg"
+          />
+          <img
+            className="icon-awesome-star-5"
+            alt="Icon awesome star"
+            src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6502d34e72965ae6a063862d/img/icon-awesome-star-1.svg"
+          />
+          <div className="text-wrapper-8">4.8</div>
+          <div className="element-reviews">10093 reviews</div>
+          <img
+            className="icon-material-2"
+            alt="Icon material"
+            src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6502d34e72965ae6a063862d/img/icon-material-navigate-next-1.svg"
+          />
+        </div>
+        <div className="overlap-5">
+          <img
+            className="revieew-lady-icon"
+            alt="Revieew lady icon"
+            src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6502d34e72965ae6a063862d/img/revieew-lady-icon-removebg-preview.png"
+          />
+          <div className="overlap-6">
+            <div className="text-wrapper-9">Rashmeet Dhindsa</div>
+            <div className="text-wrapper-10">1 Apr 20:40 pm</div>
+          </div>
+          <img
+            className="icon-awesome-star-6"
+            alt="Icon awesome star"
+            src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6502d34e72965ae6a063862d/img/icon-awesome-star-5.svg"
+          />
+          <img
+            className="icon-awesome-star-7"
+            alt="Icon awesome star"
+            src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6502d34e72965ae6a063862d/img/icon-awesome-star-6.svg"
+          />
+          <img
+            className="icon-awesome-star-8"
+            alt="Icon awesome star"
+            src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6502d34e72965ae6a063862d/img/icon-awesome-star-6.svg"
+          />
+          <img
+            className="icon-awesome-star-9"
+            alt="Icon awesome star"
+            src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6502d34e72965ae6a063862d/img/icon-awesome-star-6.svg"
+          />
+          <img
+            className="icon-awesome-star-10"
+            alt="Icon awesome star"
+            src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6502d34e72965ae6a063862d/img/icon-awesome-star-6.svg"
+          />
+          <p className="amazing-phone-fast">Amazing Phone,&nbsp;&nbsp;Fast Delivery, Must Buy</p>
+          <p className="text-wrapper-11">Xiaomi 13 Pro 5G, Ceramic Black | 12GB+256GB</p>
+        </div>
+        <div className="overlap-7">
+          <div className="rectangle-4" />
+          <div className="rectangle-5" />
+          <div className="rectangle-6" />
+          <img
+            className="image-removebg"
+            alt="Image removebg"
+            src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6502d34e72965ae6a063862d/img/image-removebg-preview--1-.png"
+          />
+          <img
+            className="line-5"
+            alt="Line"
+            src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6502d34e72965ae6a063862d/img/line-7.svg"
+          />
+          <div className="specifications">Specifications</div>
+          <img
+            className="line-6"
+            alt="Line"
+            src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6502d34e72965ae6a063862d/img/line-8.svg"
+          />
+          <div className="MIUI">MIUI 14</div>
+          <p className="storage-RAM">
+            <span className="span">
+              Storage &amp; RAM <br />
+            </span>
+            <span className="text-wrapper-12">
+              <br />
+              12GB + 256GB | LPDDR5X RAM + UFS 4.0 Storage
+              <br />
+              *Available storage and RAM are less than the
+              <br /> total memory due <br />
+              to storage of the operating system and software <br />
+              pre-installed on the device.
+            </span>
+          </p>
+          <img
+            className="line-7"
+            alt="Line"
+            src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6502d34e72965ae6a063862d/img/line-9.svg"
+          />
+          <div className="text-wrapper-13">Dimensions</div>
+          <p className="height-width">
+            Height: 162.9mm <br />
+            Width: 74.6mm <br />
+            Thickness: 8.38mm <br />
+            Weight: 229g
+          </p>
+          <p className="data-provided-by">
+            *Data provided by internal laboratories. Industry
+            <br /> measurment methods may vary, and therefore
+            <br /> actual results may differ.
+          </p>
+          <img
+            className="line-8"
+            alt="Line"
+            src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6502d34e72965ae6a063862d/img/line-9.svg"
+          />
+          <div className="text-wrapper-14">Camera</div>
+          <p className="leica-professional">
+            <span className="text-wrapper-15">
+              <br />
+              Leica professional optical lens
+              <br />
+              LEICA VARIO-SUMMICRON 1:1.9-2.2/14-75 ASPH.
+              <br />
+              <br />
+            </span>
+            <span className="text-wrapper-16">
+              Leica main camera
+              <br />
+            </span>
+            <span className="text-wrapper-15">
+              <br />
+              sensor size <br />
+              50MP <br />
+              *50MP refers to the megapixel count of the sensor,
+              <br /> acutal megapixels used in photo and video may <br />
+              vary based on different mode. <br />
+              1.6μm pixel size, 3.2 μm 4-in-1 <br />
+              Super Pixel <br />
+              f/1.9 <br />
+              HyperOIS <br />
+              IMX989 <br />
+              8P lens <br />
+              <br />
+            </span>
+            <span className="text-wrapper-16">
+              Front Camera
+              <br />
+            </span>
+            <span className="text-wrapper-17">
+              <br />
+            </span>
+            <span className="text-wrapper-15">
+              32MP in-display selfie camera
+              <br />
+              f/2.0 <br />
+              0.7μm pixel size, 1.4μm 4-in-1 Super Pixel <br />
+              89.6° FOV
+              <br />
+            </span>
+          </p>
+          <div className="text-wrapper-18">Display</div>
+          <p className="element-bit">
+            17.09cm (6.73) 10-bit AMOLED Display <br />
+            Aspect Ratio: 20:9 <br />
+            10-bit Display Panel, Over 1 Billion+ Colors <br />
+            PPI: 522 <br />
+            E6 Material
+            <br />
+            LTPO 3.0 <br />
+            240Hz Touch Sampling <br />
+            AdaptiveSync Pro <br />
+            JNCD≈0.21 Delta E≈0.28 <br />
+            1920Hz PWM Dimming <br />
+            Brightness: 1900 nits (peak) <br />
+            16000 Levels Of Brightness Adjustment <br />
+            HDR 10+ Support <br />
+            Corning® Gorilla® Glass Victus™ (Front) <br />
+            Dual Stereo Speakers <br />
+            Dolby Vision® For Display &amp; Dolby Atmos® For Sound
+            <br />
+            Hi-Res Audio (Wired &amp; Wireless) Certification
+          </p>
+          <img
+            className="line-9"
+            alt="Line"
+            src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6502d34e72965ae6a063862d/img/line-9.svg"
+          />
+          <img
+            className="line-10"
+            alt="Line"
+            src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6502d34e72965ae6a063862d/img/line-9.svg"
+          />
+          <div className="text-wrapper-19">Processor</div>
+          <p className="snapdragon-gen">
+            Snapdragon® 8 Gen 2 Mobile Platform
+            <br />
+            <br />
+            4nm power-efficient manufacturing process <br />
+            CPU: <br />
+            1x Prime core (X3-based), up to 3.19GHz <br />
+            2x Performance cores (A715-based), up to 2.8GHz <br />
+            2x Performance cores (A710-based), up to 2.8GHz <br />
+            3x Efficiency cores (A510-based), up to 2.0GHz <br />
+            Qualcomm® FastConnect™ 7800 Mobile Connectivity
+            <br /> System
+          </p>
+          <img
+            className="line-11"
+            alt="Line"
+            src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/6502d34e72965ae6a063862d/img/line-9.svg"
+          />
+          <p className="xiaomi-surge-battery">
+            Xiaomi Surge Battery Management system
+            <br />
+            Xiaomi Surge charging chipset <br />
+            Xiaomi Surge battery management chipset <br />
+            120W Xiaomi HyperCharge <br />
+            19mins to 100% (Boost mode) 24mins to 100%
+            <br /> (Standard mode) <br />
+            50W wireless turbo charging <br />
+            10W reverse wireless charging <br />
+            Inbox 120W charger <br />
+            USB type-c <br />
+            4820mAh (typ) battery
+          </p>
+          <img
+            className="line-12"
+            alt="Line"
+            src="https://cdn.animaapp.com/projects/6501de72bb8217446f7235ad/releases/65054b51fb0a69ad0d8792fc/img/line-14.svg"
+          />
+          <div className="text-wrapper-20">Battery</div>
+          <p className="dual-sim-nano-nano">
+            Dual Sim: Nano+Nano <br />
+            Supports 5G* / 4G / 3G/ 2G <br />
+            5G: Supports SA and NSA <br />
+            4G: FDD: B1/B3/B5/B8 <br />
+            3G: WCDMA: B1/B2/B5/B8 <br />
+            2G: GSM: B2/B3/B5/B8 <br />
+            Bluetooth: 5.3
+          </p>
+          <div className="network-connectivity">Network &amp; Connectivity</div>
+        </div>
+      </div>
+    </div>
+  );
+};
